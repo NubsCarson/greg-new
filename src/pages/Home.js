1255b7f24c72f923658b094f32c07dbd04abdeae
NubsCarson <nubscarson@proton.me>
2025-01-13 15:01:11 -0800

    Fixed build issues: removed unused Hero component and added babel dependency

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,13 +23,6 @@ const HomeContainer = styled.div`
   animation: ${fadeIn} 1s ease-out;
 `;
 
-const Hero = styled.section`
-  background-color: #005580;
-  padding: 2rem;
-  text-align: center;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-`;
-
 const Logo = styled.img`
   max-width: 300px;
   margin: 0 auto;
