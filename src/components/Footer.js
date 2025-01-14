3c893ce51a0a3fbe0b4230188390188f5d63b409
NubsCarson <nubscarson@proton.me>
2025-01-13 17:14:54 -0800

    Fix: Simplify Home Advisor icon filename and update reference

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -52,7 +52,7 @@ function Footer() {
     <FooterContainer>
       <Certification>
         <a href="https://www.homeadvisor.com" target="_blank" rel="noopener noreferrer">
-          <img src="/images/Home-Advisor-Icon.png" alt="Home Advisor Certified" />
+          <img src="/images/icon1.png" alt="Home Advisor Certified" />
         </a>
       </Certification>
       <Copyright>
