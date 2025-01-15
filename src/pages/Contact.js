f45500ce51960a53c68c8f65103a9d451b2aba09
Carson <nubscarson@proton.me>
2025-01-15 15:26:51 -0800

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -332,7 +332,7 @@ function Contact() {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
               </svg>
-              <a href="tel:+17024850011">(702) 485-0011</a>
+              <a href="tel:+17024970477">(702) 497-0477</a>
             </ContactItem>
             <ContactItem>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
